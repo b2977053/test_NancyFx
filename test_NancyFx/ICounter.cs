bdbaba8c5f50dd0a2d18e2bf1cfaf792b30d8bc5
steven <b2977053@venus.lit.edu.tw>
2020-01-10 13:52:31 +0800

    加入專案檔案。

---
+++ test_NancyFx/ICounter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace test_NancyFx
+{
+    interface ICounter
+    {
+    }
+
+    public class Counter: ICounter
+    {
+        public double cpuCounter { get; set; }
+        public double memoryCounter { get; set; }
+
+        public Counter()
+        {
+            /*
+             * 程式範例－使用 C# 查詢 CPU 與記憶體使用狀況-黑暗執行緒
+             * https://blog.darkthread.net/blog/performancecounter-c-sample/
+             */
+            PerformanceCounter cpu = new PerformanceCounter("Processor"
+            , "% Processor Time"
+            , "_Total");
+            PerformanceCounter memory = new PerformanceCounter("Memory"
+            , "% Committed Bytes in Use");
+
+            Queue<float> cpuCounters = new Queue<float>();
+            Queue<float> memoryCounters = new Queue<float>();
+            float total_cpu = 0;
+            float total_memory = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                float cpuCounter = cpu.NextValue();
+                total_cpu += cpuCounter;
+                float memoryCounter = memory.NextValue();
+                total_memory += memoryCounter;
+
+                cpuCounters.Enqueue(cpuCounter);
+                memoryCounters.Enqueue(memoryCounter);
+                Thread.Sleep(100);
+            }
+
+            cpuCounter = System.Math.Round(total_cpu / cpuCounters.Count * 10) / 10;
+            memoryCounter = System.Math.Round(total_memory / memoryCounters.Count * 10) / 10;
+        }
+    }
+}
---
+++ test_NancyFx/Program.cs
@@ -0,0 +1,56 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+
+namespace test_NancyFx
+{
+    class Program
+    {
+        /*
+         * NancyFx－打造小型 WebAPI 與 Microservice 的輕巧利器-黑暗執行緒
+         * https://blog.darkthread.net/blog/nancyfx
+         * 
+         * Nancy.Hosting.Self
+         * 
+         * 開放授權  netsh http add urlacl url="http://+:9527/" user="Everyone"
+         * 移除權限  netsh http delete urlacl url="http://+:9527/"
+         */
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(
+                    new Uri("http://localhost:9527"))) // http://127.0.0.1:9527/
+            {
+                try
+                {
+                    host.Start();
+                    Console.WriteLine("Press any key to stop...");
+                    Console.Read();
+                    host.Stop();
+                }
+                catch (AutomaticUrlReservationCreationFailureException ex)
+                {
+                    Console.WriteLine("權限不足:" + ex.Message);
+                    Console.WriteLine("Press any key to stop...");
+                    Console.Read();
+
+                }
+            }
+        }
+
+        
+    }
+
+    public class GuidGeneratorModule : NancyModule
+    {
+        public GuidGeneratorModule()
+        {
+            Get("/", (p) =>
+            {
+                Counter counter = new Counter();
+                return Response.AsJson(counter);
+            });
+        }
+    }
+
+
+}
